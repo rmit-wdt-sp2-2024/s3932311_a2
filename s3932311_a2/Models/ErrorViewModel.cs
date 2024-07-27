ee0bb5fc604c5264574115af597ac00f60f2be14
ChristoPyper <136978994+ChristoPyper@users.noreply.github.com>
2024-07-28 00:08:52 +0200

    Updated to MVC design

---
+++ s3932311_a2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace s3932311_a2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

893f61d1578b9efcc79fa00b2b494fc1f23a368f
ChristoffelPyper <141031664+ChristoffelPyper@users.noreply.github.com>
2024-08-10 11:59:39 +0200

    Delete s3932311_a2 directory

--- s3932311_a2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace s3932311_a2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
